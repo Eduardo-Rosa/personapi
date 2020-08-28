6372bbf6e010d700e014075dfeaeafd039135a75
Eduardo-Rosa <edu.betim@aol.com>
2020-08-27 22:29:04 -0300

    Developed get person by id and find person by id

---
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PhoneDTO.java
@@ -0,0 +1,40 @@
+package one.digitalinnovation.personapi.dto.request;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class PhoneDTO {
+
+	
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	private PhoneType type;
+	
+	@NotEmpty
+	@Size(min = 13, max = 15)
+	private String number;
+	
+	
+	
+	
+}
